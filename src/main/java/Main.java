6929c5909b8d425378b2511fc5623d4a9730861f
nik220822 <121189416+nik220822@users.noreply.github.com>
2023-04-24 09:18:30 +0300

    Using future callable and executorService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,57 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 
 public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
+        List<Future> futures = new ArrayList<>();
         String[] texts = new String[25];
-
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
-
         long startTs = System.currentTimeMillis(); // start time
 
         for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
+            Callable<Integer> callable = new Callable<>() {
+                @Override
+                public Integer call() {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
                             }
                         }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
                     }
+                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                    return maxSize;
                 }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            thread.start();
-            threads.add(thread);
-        }
-        for (Thread thread : threads) {
-            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+            };
+            Future<Integer> future = executorService.submit(callable);
+            futures.add(future);
         }
 
+        List<Integer> results = new ArrayList<>();
+        for (Future future : futures) {
+            results.add((Integer) future.get());
+        }
+        executorService.shutdown();
+        System.out.println(results.stream()
+                .max(Comparator.naturalOrder())
+                .get());
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }
